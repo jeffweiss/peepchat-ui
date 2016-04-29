78549d2784cc9968d10ec67cfe72099ab28b2ede
Jeff Weiss <jeff.weiss@puppet.com>
2016-04-29 11:37:03 -0700

    add flash messages

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -4,11 +4,24 @@ const { Route, inject } = Ember;
 
 export default Route.extend({
   session: inject.service(),
+  flashMessages: inject.service(),
   actions: {
     doLogin() {
       const user = this.get('currentModel');
       this.get('session')
-        .authenticate('authenticator:peepchat', user.email, user.password);
+        .authenticate('authenticator:peepchat', user.email, user.password)
+        .then(() => {
+          this.get('flashMessages').success('Logged in!');
+        }).catch((response) => {
+          const { errors } = response;
+          console.log(errors);
+
+          if (errors.mapBy('code').indexOf(401) >= 0) {
+            this.get('flashMessages').danger('There was a problem with your username or password, please try again');
+          } else {
+            this.get('flashMessages').danger('Server Error');
+          }
+        });
     }
   },
   model() {
